27adcda2a560b10d7bf33c86e16773e7890745e6
DWZ753 <1369837512@qq.com>
2025-10-09 13:01:24 +0800

    提交了week1的作业

---
+++ week 1/code/HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int x;
+	scanf("%d",&x);
+	while(x>=0){
+		if(x==0){
+			printf("helloworld\n");
+		}else if(x==1){
+			printf("HELLOWORLD\n");
+		}scanf("%d",&x);
+	}
+	
+	return 0;
+} 
--- week 1/helloworld.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a
-	int b
-	printf("请输入两个数字")
-	return 0;
-}
