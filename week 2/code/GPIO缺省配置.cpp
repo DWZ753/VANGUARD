bf0710d6ac08c5e923f4ee88d42099bd49398c10
DWZ753 <1369837512@qq.com>
2025-10-16 23:31:15 +0800

    优化了初始化函数

--- week 2/code/GPIO缺省配置.cpp
+++ week 2/code/GPIO缺省配置.cpp
@@ -1,19 +1,23 @@
 #include<stdio.h>
-enum GPIO_Speed_Type{
-	GPIO_Speed_2MHz,GPIO_Speed_10MHz,GPIO_Speed_50MHz
-};
-struct GPIO{
-	enum GPIO_Speed_Type GPIO_Speed; 
-};
-void GPIO_StructureInit(struct GPIO *p)
-{
-	p->GPIO_Speed=GPIO_Speed_2MHz;
-}
+
+enum GPIO_Speed_Hz{
+	GPIO_Speed_2MHz,GPIO_Speed_10MHz,GPIO_Speed_50MHz};
+	
+struct GPIO_parameter{
+	enum GPIO_Speed_Hz GPIO_Speed;
+	};
+
+void GPIO_StructureInit(struct GPIO_parameter *p);
 
 int main()
 {
-	struct GPIO test;
+	struct GPIO_parameter test;
 	GPIO_StructureInit(&test);
 	
 	return 0;
+}
+
+void GPIO_StructureInit(struct GPIO_parameter *p)
+{
+	p->GPIO_Speed=GPIO_Speed_2MHz;
 }
